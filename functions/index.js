43362070721e3a7a9b17c9b0bd1c4c7990ba3629
WonyeongChoi <coma01155@gmail.com>
2021-02-08 23:43:16 +0900

    feat: Implement auth functions

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,23 @@
 // eslint-disable-next-line no-unused-vars
-const functions = require("firebase-functions");
+var functions = require("firebase-functions")
+var admin = require("firebase-admin")
+var serviceAccount = require("./kalgory-admin-key.json")
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+})
+const fdb = admin.firestore()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.createUser = functions.auth.user().onCreate( (user) => {
+    const u = {
+        uid: user.uid,
+        user_id: "",
+        created_at: new Date(user.metadata.creationTime).valueOf(),
+        rank: 0
+    }
+    return fdb.collection("USER").doc(user.uid).set(u)
+})
+
+exports.deleteUser = functions.auth.user().onDelete((user) => {
+    const { uid } = user
+    return fdb.collection("USER").doc(uid).delete()
+})
