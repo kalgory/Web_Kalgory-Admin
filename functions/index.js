fce4b9e2d2a96ba63379bf20efefb2b725408539
WonyeongChoi <coma01155@gmail.com>
2021-02-12 23:33:11 +0900

    feat: Implement user auth functions
    - onUserCreate: Function that triggers when an Firebase user is created
    - onUserDelete: Function that triggers when an Firebase user is deleted

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,21 @@
-// eslint-disable-next-line no-unused-vars
-var functions = require("firebase-functions")
-var admin = require("firebase-admin")
-var serviceAccount = require("./kalgory-admin-key.json")
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-})
-const fdb = admin.firestore()
+const Functions = require("firebase-functions")
+const Admin = require("firebase-admin")
+Admin.initializeApp();
 
-exports.createUser = functions.auth.user().onCreate( (user) => {
-    const u = {
+exports.onUserCreate = Functions.auth.user().onCreate( (user) => {
+    return Admin.firestore().collection("USER").add({
         uid: user.uid,
-        user_id: "",
-        created_at: new Date(user.metadata.creationTime).valueOf(),
+        created_at: new Date(user.metadata.creationTime),
         rank: 0
-    }
-    return fdb.collection("USER").doc(user.uid).set(u)
+    })
 })
 
-exports.deleteUser = functions.auth.user().onDelete((user) => {
+exports.onUserDelete = Functions.auth.user().onDelete((user) => {
     const { uid } = user
-    return fdb.collection("USER").doc(uid).delete()
+    const userQuery = Admin.firestore().collection("USER").doc().where("uid", "==", uid)
+    userQuery.get().then(function (querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            doc.ref.delete()
+        })
+    })
 })
