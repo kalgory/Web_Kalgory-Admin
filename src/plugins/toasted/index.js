55c74a2225e2876d421d30f2a91ec427d8c91457
WonyeongChoi <coma01155@gmail.com>
2021-02-15 14:50:23 +0900

    feat: Add vue-toasted options

--- src/plugins/toasted/index.js
+++ src/plugins/toasted/index.js
@@ -1,4 +1,13 @@
 import Vue from 'vue';
 import Toasted from 'vue-toasted';
 
-Vue.use(Toasted);
+const Options = {
+  singleton: true,
+  duration: 5000,
+  keepOnHover: true,
+  theme: 'outline',
+  position: 'top-center',
+  iconPack: 'mdi',
+};
+
+Vue.use(Toasted, Options);
