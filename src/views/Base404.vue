3655619bde0c6a6cfa0887d43df7b1eee7767591
Wonyeong Choi <coma01155@gmail.com>
2021-02-18 17:54:20 +0900

    feat: add 404 error view component

---
+++ src/views/Base404.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container class="fill-height">
+    <v-row
+      justify="center"
+      class="text-center"
+    >
+      <v-col>
+        <p class="text-h1">
+          404
+        </p>
+        <p>
+          Please check that the URL you entered is correct
+          , or click the button below to return to the homepage
+        </p>
+        <div>
+          <v-btn href="/">
+            Back to Home
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Base404',
+};
+</script>
