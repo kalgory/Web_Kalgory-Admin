3474368ab76ce535207298ab2d2e793c8a49d82b
Wonyeong Choi <coma01155@gmail.com>
2021-02-23 02:38:51 +0900

    feat: improve security

--- src/permission.js
+++ src/permission.js
@@ -1,19 +1,50 @@
-import { getToken } from '@/plugins/token';
+import { getToken, removeToken } from '@/plugins/token';
+import Firebase from 'firebase/app';
 import router from './router';
 
 const whiteList = ['/signin'];
 
 router.beforeEach(async (to, from, next) => {
-  const hasToken = getToken('__session');
-  if (hasToken) {
-    if (to.path === '/signin') {
-      next({ path: '/dashboard' });
-    } else {
+  Firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      user.getIdToken(false)
+        .then((token) => {
+          if (token === getToken()) {
+            if (to.path === '/signin') {
+              next({ path: '/dashboard' });
+            } else {
+              next();
+            }
+          } else if (whiteList.indexOf(to.path) !== -1) {
+            next();
+          } else {
+            alert('다시 로그인해주세요');
+            removeToken();
+            next('/signin');
+          }
+        })
+        .catch((error) => {
+          this.$toasted.global.error({ message: error.message });
+          removeToken();
+          next('/signin');
+        });
+    } else if (whiteList.indexOf(to.path) !== -1) {
+      // 정상
       next();
+    } else {
+      next('/signin');
     }
-  } else if (whiteList.indexOf(to.path) !== -1) {
-    next();
-  } else {
-    next('signin');
-  }
+  });
+  // console.log(hasToken);
+  // if (hasToken === getToken) {
+  //   if (to.path === '/signin') {
+  //     next({ path: '/dashboard' });
+  //   } else {
+  //     next();
+  //   }
+  // } else if (whiteList.indexOf(to.path) !== -1) {
+  //   next();
+  // } else {
+  //   next('/signin');
+  // }
 });
