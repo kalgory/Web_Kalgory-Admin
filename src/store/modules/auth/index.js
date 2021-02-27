b3f75bcdcb2146f6a98b60c688962b8ebe02b352
Wonyeong Choi <coma01155@gmail.com>
2021-02-27 17:45:48 +0900

    fix: fix progress bar error

--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -1,15 +1,22 @@
 export default {
   state: {
+    isAuthLoading: true,
     user: null,
   },
 
   getters: {
+    getIsAuthLoading(state) {
+      return state.isAuthLoading;
+    },
     getUser(state) {
       return state.user;
     },
   },
 
   mutations: {
+    setIsAuthLoading(state, value) {
+      state.isAuthLoading = value;
+    },
     setUser(state, value) {
       state.user = value;
     },
