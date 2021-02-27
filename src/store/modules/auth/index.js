4e81562bc5d4b414e554af4cb3683ba19d4ff2b9
Wonyeong Choi <coma01155@gmail.com>
2021-02-27 16:11:48 +0900

    feat: keep user state only

--- src/store/modules/auth/index.js
+++ src/store/modules/auth/index.js
@@ -1,29 +1,15 @@
 export default {
   state: {
-    isAuthLoading: true,
-    isAuthenticated: false,
     user: null,
   },
 
   getters: {
-    getIsAuthLoading(state) {
-      return state.isAuthLoading;
-    },
-    getIsAuthenticated(state) {
-      return state.isAuthenticated;
-    },
     getUser(state) {
       return state.user;
     },
   },
 
   mutations: {
-    setIsAuthLoading(state, value) {
-      state.isAuthLoading = value;
-    },
-    setIsAuthenticated(state, value) {
-      state.isAuthenticated = value;
-    },
     setUser(state, value) {
       state.user = value;
     },
