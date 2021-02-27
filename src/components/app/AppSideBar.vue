ffe80208f4774005fb853ed8ac5657f9ef5aeaa6
Wonyeong Choi <coma01155@gmail.com>
2021-02-27 15:21:13 +0900

    style: improve code readability
    create BarMenu component

--- src/components/app/AppSideBar.vue
+++ src/components/app/AppSideBar.vue
@@ -24,28 +24,16 @@
 
     <v-divider />
 
-    <v-list dense>
-      <v-list-item
-        v-for="item in items"
-        :key="item.title"
-        :to="item.path"
-      >
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
+    <bar-menu />
   </v-navigation-drawer>
 </template>
 
 <script>
+import BarMenu from '@/components/app/bar/BarMenu.vue';
+
 export default {
   name: 'AppSideBar',
-
+  components: { BarMenu },
   props: {
     value: {
       type: Boolean,
@@ -60,33 +48,6 @@ export default {
     return {
       localDrawer: this.value,
       clipped: false,
-      items: [
-        {
-          title: 'Home',
-          icon: 'mdi-home-variant',
-          path: '/dashboard',
-        },
-        {
-          title: 'User',
-          icon: 'mdi-account',
-          path: '/user',
-        },
-        {
-          title: 'Concept',
-          icon: 'mdi-head-dots-horizontal-outline',
-          path: '/concept',
-        },
-        {
-          title: 'Community',
-          icon: 'mdi-account-group',
-          path: 'community',
-        },
-        {
-          title: 'Problem',
-          icon: 'mdi-book-open-outline',
-          path: 'problem',
-        },
-      ],
     };
   },
 
---
+++ src/components/app/bar/BarMenu.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-list dense>
+    <v-list-item
+      v-for="item in items"
+      :key="item.title"
+      :to="item.path"
+    >
+      <v-list-item-icon>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-icon>
+
+      <v-list-item-content>
+        <v-list-item-title>{{ item.title }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: 'AppMenu',
+
+  data() {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'mdi-home-variant',
+          path: '/dashboard',
+        },
+        {
+          title: 'User',
+          icon: 'mdi-account',
+          path: '/user',
+        },
+        {
+          title: 'Concept',
+          icon: 'mdi-head-dots-horizontal-outline',
+          path: '/concept',
+        },
+        {
+          title: 'Community',
+          icon: 'mdi-account-group',
+          path: 'community',
+        },
+        {
+          title: 'Problem',
+          icon: 'mdi-book-open-outline',
+          path: 'problem',
+        },
+      ],
+    };
+  },
+};
+</script>
